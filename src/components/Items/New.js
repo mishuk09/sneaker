9c8c9b511da6b52540f7bcb76c03cb5034dca245
mishuk09 <mishukinfo09@gmail.com>
2024-05-26 21:24:44 +0530

    update front end code

--- src/components/Items/New.js
+++ src/components/Items/New.js
@@ -26,25 +26,51 @@ const New = () => {
                     <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
                 </div>
             ) : (
+                // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                //     {posts.map(product => (
+                //         <Link to={`/product/${product._id}`} key={product._id} className="bg-white p-2 rounded-lg shadow-md">
+                //             <img src={product.img} alt={product.title} className="w-full rounded-sm h-[280px] object-cover mb-4" />
+                //             <h2 className="text-[22px] font-semibold">{product.title}</h2>
+                //             <div className="flex justify-between items-center mb-2 mt-2">
+                //                 <div className='flex items-center'>
+                //                     <span className="me-4 font-semibold text-red-500">रू {product.newPrice}</span>
+                //                     <span className="text-sm font-semibold text-gray-700 line-through">रू {product.oldPrice}</span>
+                //                 </div>
+                //                 <div>
+                //                     <span className="text-green-500 font-bold mb-4">
+                //                         {Math.round(((product.newPrice - product.oldPrice) / product.oldPrice) * 100)}% OFF
+                //                     </span>
+                //                 </div>
+                //             </div>
+                //         </Link>
+                //     ))}
+                // </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {posts.map(product => (
-                        <Link to={`/product/${product._id}`} key={product._id} className="bg-white p-2 rounded-lg shadow-md">
-                            <img src={product.img} alt={product.title} className="w-full rounded-sm h-[280px] object-cover mb-4" />
-                            <h2 className="text-[22px] font-semibold">{product.title}</h2>
-                            <div className="flex justify-between items-center mb-2 mt-2">
+                        <Link to={`/product/${product._id}`} key={product._id} className="bg-white rounded-lg shadow-md p-4  ">
+                            <div className="overflow-hidden rounded-md">
+                                <img src={product.img} alt={product.title} className="w-full h-48 object-cover mb-4 transform hover:scale-110 transition-transform duration-300" />
+                            </div>
+                            <h2 className="text-lg font-semibold text-gray-900">
+                                {product.title.toLowerCase().replace(/\b\w/g, c => c.toUpperCase()).substring(0, 40)}{product.title.length > 40 ? '...' : ''}
+                            </h2>
+                            <div className="flex justify-between items-center mt-2">
                                 <div className='flex items-center'>
-                                    <span className="me-4 font-semibold text-red-500">रू {product.newPrice}</span>
+                                    <span className="mr-4 text-lg font-semibold text-red-500">रू {product.newPrice}</span>
                                     <span className="text-sm font-semibold text-gray-700 line-through">रू {product.oldPrice}</span>
                                 </div>
                                 <div>
-                                    <span className="text-green-500 font-bold mb-4">
-                                        {Math.round(((product.newPrice - product.oldPrice) / product.oldPrice) * 100)}% OFF
+                                    <span className="text-green-500 font-bold">
+                                        {Math.round(((product.oldPrice - product.newPrice) / product.oldPrice) * 100)}% OFF
                                     </span>
                                 </div>
                             </div>
                         </Link>
                     ))}
                 </div>
+
+
+
             )}
         </div>
     );

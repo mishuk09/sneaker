def48f8d3e6d9ab15d590c5805609bd3a19a9d52
mishuk09 <mishukinfo09@gmail.com>
2024-05-27 16:16:30 +0530

    update navbar

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,7 +16,7 @@ const Cart = ({ isOpen, toggleCart }) => {
             <div className="relative">
                 {/* Cart Panel */}
                 <div
-                    className={`fixed top-0 right-0 w-[500px] h-full bg-white shadow-lg transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-50 flex flex-col`}
+                    className={`fixed top-0 right-0 w-full md:w-[500px] h-full bg-white shadow-lg transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-50 flex flex-col`}
                 >
                     <div className="p-4 flex-grow overflow-y-auto">
                         <h2 className="text-lg font-semibold mb-4">Your Cart</h2>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import mainlogo from '../mainlogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faMagnifyingGlass, faUser, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import Cart from './Cart'; // Import the Cart component
 
 const Navbar = ({ toggleCart, isCartOpen }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
         setIsAuthenticated(!!token);
     }, []);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     return (
         <div>
@@ -22,29 +27,49 @@ const Navbar = ({ toggleCart, isCartOpen }) => {
                             <img className='w-12' src={mainlogo} alt="Logo" />
                         </Link>
                     </div>
-                    <div className="navbar-menu">
-                        <ul className="flex">
-                            <li className='me-4 font-semibold'><Link to='/' >New</Link></li>
-                            <li className='me-4 font-semibold'><Link to='/shoes' >Shoes</Link></li>
-                            <li className='me-4 font-semibold'><Link to='/cloth'>Clothes</Link></li>
-                            <li className='me-4 font-semibold'><Link to='/glovs' >Gloves</Link></li>
-                            <li className='me-4 font-semibold'><Link to='/kitchen'>Kitchen</Link></li>
-                        </ul>
+                    <div className="flex items-center">
+                        <div className="navbar-icons flex gap-6 lg:hidden">
+                            <div><FontAwesomeIcon size='xl' icon={faMagnifyingGlass} /></div>
+                            <div onClick={toggleCart} className="cursor-pointer">
+                                <FontAwesomeIcon size='xl' icon={faCartShopping} />
+                            </div>
+                            <div>
+                                <Link to={isAuthenticated ? '/dashboard' : '/signin'}>
+                                    <FontAwesomeIcon size='xl' icon={faUser} />
+                                </Link>
+                            </div>
+                        </div>
+                        <button className="lg:hidden ml-4" onClick={toggleMobileMenu}>
+                            <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} size="xl" />
+                        </button>
                     </div>
-                    <div className="navbar-icons flex gap-6">
+                    <div className="navbar-icons hidden lg:flex gap-6">
                         <div><FontAwesomeIcon size='xl' icon={faMagnifyingGlass} /></div>
                         <div onClick={toggleCart} className="cursor-pointer">
                             <FontAwesomeIcon size='xl' icon={faCartShopping} />
                         </div>
                         <div>
                             <Link to={isAuthenticated ? '/dashboard' : '/signin'}>
-                            {/* <Link to='/signin'> */}
                                 <FontAwesomeIcon size='xl' icon={faUser} />
                             </Link>
                         </div>
                     </div>
                 </div>
             </div>
+            {isMobileMenuOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-75 z-50 flex flex-col items-center justify-center lg:hidden">
+                    <button className="absolute top-4 right-4 text-white" onClick={toggleMobileMenu}>
+                        <FontAwesomeIcon icon={faTimes} size="2x" />
+                    </button>
+                    <ul className="flex flex-col items-center space-y-6">
+                        <li className='text-white text-xl font-semibold'><Link to='/' onClick={toggleMobileMenu}>New</Link></li>
+                        <li className='text-white text-xl font-semibold'><Link to='/shoes' onClick={toggleMobileMenu}>Shoes</Link></li>
+                        <li className='text-white text-xl font-semibold'><Link to='/cloth' onClick={toggleMobileMenu}>Clothes</Link></li>
+                        <li className='text-white text-xl font-semibold'><Link to='/gloves' onClick={toggleMobileMenu}>Gloves</Link></li>
+                        <li className='text-white text-xl font-semibold'><Link to='/kitchen' onClick={toggleMobileMenu}>Kitchen</Link></li>
+                    </ul>
+                </div>
+            )}
             <Cart isOpen={isCartOpen} toggleCart={toggleCart} /> {/* Add the Cart component */}
         </div>
     );
